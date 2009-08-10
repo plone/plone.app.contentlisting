5e382d39524303d4520143dff9de0c7f88ec43f2
Geir Baekholt <geir@baekholt.com>
2009-08-10 13:30:21 +0000

    Add the neccesary types filter to hide implementation details like criterion and form elements from search results. Oh how i hate that we need this.
    svn path=/plone.app.contentlisting/trunk/; revision=28632

--- plone/app/contentlisting/browser.py
+++ plone/app/contentlisting/browser.py
@@ -1,8 +1,7 @@
 from Products.Five.browser import BrowserView
 from interfaces import IContentListing
 from Products.CMFCore.utils import getToolByName
-
-
+import types
 
 
 class FolderListing(BrowserView):
@@ -39,10 +38,29 @@ class SearchResults(BrowserView):
     def __call__(self, **kw):
         query = {}
         query.update(kw)
+        if not kw:
+            query.update(getattr(self.request, 'form',{}))
+            query.update(dict(getattr(self.request, 'other',{})))
         catalog = getToolByName(self.context, 'portal_catalog')
         return IContentListing(catalog(query))
 
 
 
 
-
+    def ensureFriendlyTypes(self):
+        # ported from Plone's queryCatalog. It hurts to bring this one along. 
+        # The fact that it is needed tells us someone used types where they 
+        # should not have. 
+        # please remove this one when it is no longer needed.
+        
+        ploneUtils = getToolByName(context, 'plone_utils')
+        portal_type = query.get('portal_type', [])
+        if not type(portal_type) is types.ListType:
+            portal_type = [portal_type]
+        Type = query.get('Type', [])
+        if not type(Type) is types.ListType:
+            Type = [Type]
+        typesList = portal_type + Type
+        if not typesList:
+            friendlyTypes = ploneUtils.getUserFriendlyTypes(typesList)
+            query['portal_type'] = friendlyTypes
