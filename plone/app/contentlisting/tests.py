f1fe71ccdaaa7fec3bbd97a4daf999a7151b9952
Geir Baekholt <geir@baekholt.com>
2009-08-11 17:50:57 +0000

    remove borked redundant tests
    svn path=/plone.app.contentlisting/trunk/; revision=28695

--- plone/app/contentlisting/tests.py
+++ plone/app/contentlisting/tests.py
@@ -8,53 +8,25 @@ from Products.Five import zcml
 from Products.Five import fiveconfigure
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import PloneSite
-ptc.setupPloneSite()
 
 from interfaces import IContentListing, IContentListingObject
 import plone.app.contentlisting
-import plone.app.layout
 from zope.interface.verify import verifyObject 
+
+ptc.setupPloneSite()
 
 class TestCase(ptc.PloneTestCase):
     class layer(PloneSite):
         @classmethod
         def setUp(cls):
             fiveconfigure.debug_mode = True
-            zcml.load_config('configure.zcml',
-                             plone.app.layout)
-            zcml.load_config('configure.zcml',
-                             plone.app.contentlisting)
-            fiveconfigure.debug_mode = True
+            zcml.load_config('configure.zcml', plone.app.contentlisting)
+            fiveconfigure.debug_mode = False
 
         @classmethod
         def tearDown(cls):
             pass
 
-class TestCatalogInterface(TestCase):
-        #   - self.portal is the portal root
-        #   - self.folder is the current user's folder
-        #   - self.logout() "logs out" so that the user is Anonymous
-        #   - self.setRoles(['Manager', 'Member']) adjusts the roles of the current user
-    
-#    def makeTestObject(self): 
-#        """Returns an ISample instance""" 
-#        return IContentListingFactory(self.folder)()
-#    
-#    def test_portal_title(self):
-#        # testing that my tests run
-#        self.assertEquals("Plone site", self.portal.getProperty('title'))
-#
-#    def testListingImplementsInterface(self):
-#        self.failUnless(verifyObject(IContentListing, self.makeTestObject()))
-#
-#    def testListingObjectsImplementsInterface(self):
-#        self.failUnless(verifyObject( IContentListingObject, self.makeTestObject()[0]))
-#        
-#    def testFolderContents(self):
-#        listing = self.portal.folderListing()
-#        self.failUnless(verifyObject( IContentListing, listing))
-#        print listing
-    pass
         
         
 def test_suite():
@@ -80,7 +52,7 @@ def test_suite():
     #    test_class=TestCase),
 
     ])
-    suite.addTest(unittest.makeSuite(TestCatalogInterface))
+    #suite.addTest(unittest.makeSuite(TestCatalogInterface))
     return suite 
     
     
