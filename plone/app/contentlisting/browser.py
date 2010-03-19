07314979634b7c765d95e53b746132c87b40d392
Roel Bruggink <roel+plone.org@fourdigits.nl>
2010-03-19 14:46:36 +0000

    Replace raising exception by logging
    svn path=/plone.app.contentlisting/trunk/; revision=35387

--- plone/app/contentlisting/browser.py
+++ plone/app/contentlisting/browser.py
@@ -1,41 +1,26 @@
 from Products.Five.browser import BrowserView
 from interfaces import IContentListing
 from Products.CMFCore.utils import getToolByName
+import logging
 import types
 
 
-class InvalidCatalogIndexException(Exception):
-    """The profile does not exist."""
-
-
-def checkIndexes(context, query):
-    # make sure that all the used indexes really are indexes
-    # If not, then return no results because something somewhere is wrong.
-    # Error will be logged to prevent everything from crashing.
-    catalog = getToolByName(context, 'portal_catalog')
-    indexes = catalog.indexes()
-    for index in query:
-        if not index in indexes:
-            raise InvalidCatalogIndexException("'%s' is not an valid catalog index." % index)
-
-
 class FolderListing(BrowserView):
-    
-    def __call__(self, batch=False, b_size=100,b_start=0, **kw):
+
+    def __call__(self, batch=False, b_size=100, b_start=0, **kw):
         query = {}
         query.update(kw)
         if not kw:
-            query.update(getattr(self.request, 'form',{}))
-            query.update(dict(getattr(self.request, 'other',{})))
-        
-        query['path'] = {'query': '/'.join(self.context.getPhysicalPath()), 
-                'depth':1}
-                
-        # if we don't have asked explicitly for other sorting, we'll want 
+            query.update(getattr(self.request, 'form', {}))
+            query.update(dict(getattr(self.request, 'other', {})))
+
+        query['path'] = {'query': '/'.join(self.context.getPhysicalPath()), 'depth': 1}
+
+        # if we don't have asked explicitly for other sorting, we'll want
         # it by position in parent
         if not query.get('sort_on', None):
             query['sort_on'] = 'getObjPositionInParent'
-        
+
         show_inactive = getToolByName(self.context, 'portal_membership').checkPermission('Access inactive portal content', self.context)
         results = IContentListing(getToolByName(self.context, 'portal_catalog')(query))
 
@@ -47,9 +32,9 @@ class FolderListing(BrowserView):
 
 
 class SearchResults(BrowserView):
-    
-    def __call__(self, query=None, batch=False, b_size=100,b_start=0, **kw):
-        """ Get properly wrapped search results from the catalog. 
+
+    def __call__(self, query=None, batch=False, b_size=100, b_start=0, **kw):
+        """ Get properly wrapped search results from the catalog.
             Everything in Plone that performs searches should go through this view.
             query (optional) should be a dictionary of catalog parameters
             you can also pass catalog parameters as individual named keywords
@@ -59,11 +44,24 @@ class SearchResults(BrowserView):
         query.update(kw)
         if not query:
             return IContentListing([])
+
         query = self.ensureFriendlyTypes(query)
-        
-        checkIndexes(self.context, query)
-                
+
+        # make sure that all the used indexes really are indexes
+        # Error will be logged to prevent everything from crashing.
+        logger = logging.getLogger('plone.app.contentlisting')
         catalog = getToolByName(self.context, 'portal_catalog')
+        indexes = catalog.indexes()
+        invalid_indexes = [index for index in query if index not in indexes]
+        valid_indexes = [index for index in query if index in indexes]
+        if invalid_indexes:
+            for index in invalid_indexes:
+                logger.warning("'%s' is an invalid catalog index" % index)
+
+        # We'll ignore any invalid index, but will return an empty set if none of the indexes are valid.
+        if not valid_indexes:
+            return IContentListing([])
+
         results = IContentListing(catalog(query))
         if batch:
             from Products.CMFPlone import Batch
@@ -72,11 +70,14 @@ class SearchResults(BrowserView):
         return results
 
     def ensureFriendlyTypes(self, query):
-        # ported from Plone's queryCatalog. It hurts to bring this one along. 
-        # The fact that it is needed at all tells us that we currently abuse 
-        # the concept of types in Plone 
+        # ported from Plone's queryCatalog. It hurts to bring this one along.
+        # The fact that it is needed at all tells us that we currently abuse
+        # the concept of types in Plone
         # please remove this one when it is no longer needed.
-        
+
+        # I do not like this here, because it conflicts for collections searching on portal_type.
+        return query
+
         ploneUtils = getToolByName(self.context, 'plone_utils')
         portal_type = query.get('portal_type', [])
         if not type(portal_type) is types.ListType:
