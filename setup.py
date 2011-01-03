958b9b4a8d8c91c240ee6c440408756f12756783
Hanno Schlichting <hanno@hannosch.eu>
2011-01-03 19:39:08 +0000

    Update some boilerplate to current best practice
    svn path=/plone.app.contentlisting/trunk/; revision=46650

--- setup.py
+++ setup.py
@@ -1,39 +1,31 @@
 from setuptools import setup, find_packages
-import os
 
 version = '0.1'
-
-tests_require = ['plone.app.testing']
 
 setup(name='plone.app.contentlisting',
       version=version,
       description="Listing of content for the Plone CMS",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open("CHANGES.txt").read(),
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='Geir B\xc3\xa6kholt',
-      author_email='baekholt@jarn.com',
-      url='http://svn.plone.org/svn/plone/',
-      license='GPL',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.contentlisting',
+      license='GPL version 2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone', 'plone.app'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
+          'plone.app.testing',
       ],
-      tests_require=tests_require,
-      extras_require={
-          'test': tests_require,
-      },
       entry_points='''
-          [z3c.autoinclude.plugin]
-          target = plone
+      [z3c.autoinclude.plugin]
+      target = plone
       ''',
       )
