555d38f6f4bec50b1606eb8f1b7f986443a40262
Hanno Schlichting <hanno@hannosch.eu>
2011-01-06 12:49:04 +0000

    Correct Batch import, the class is only available via the __init__ after the initialize function has run, not yet during normal import time
    svn path=/plone.app.contentlisting/trunk/; revision=46774

--- plone/app/contentlisting/browser.py
+++ plone/app/contentlisting/browser.py
@@ -1,5 +1,5 @@
 from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone import Batch
+from Products.CMFPlone.PloneBatch import Batch
 from zope.publisher.browser import BrowserView
 
 from .interfaces import IContentListing
