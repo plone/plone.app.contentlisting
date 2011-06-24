b30bbaee9e964b97744461b238cd46dc68b5f511
Hanno Schlichting <hanno@hannosch.eu>
2011-06-24 12:12:41 +0000

    Import order cleanup
    svn path=/plone.app.contentlisting/trunk/; revision=50668

--- plone/app/contentlisting/tests/base.py
+++ plone/app/contentlisting/tests/base.py
@@ -1,10 +1,11 @@
 import unittest2 as unittest
 
-from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import PLONE_FIXTURE
-from plone.app.testing import TEST_USER_ID, setRoles
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
 from Products.CMFCore.utils import getToolByName
 from zope.configuration import xmlconfig
 
