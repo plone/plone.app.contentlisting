0143662b28f3a54e7268cd0cb10a9aa1fb517288
Maurits van Rees <maurits@vanrees.org>
2018-01-17 12:55:13 +0100

    Reindented setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,33 +2,38 @@ from setuptools import setup, find_packages
 
 version = '1.0.7.dev0'
 
-setup(name='plone.app.contentlisting',
-      version=version,
-      description="Listing of content for the Plone CMS",
-      long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.txt").read(),
-      classifiers=[
+long_description = '{0}\n\n{1}'.format(
+    open('README.txt').read(),
+    open('CHANGES.txt').read()
+)
+
+setup(
+    name='plone.app.contentlisting',
+    version=version,
+    description="Listing of content for the Plone CMS",
+    long_description=long_description,
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://pypi.python.org/pypi/plone.app.contentlisting',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.uuid',
-      ],
-      extras_require={
+    ],
+    keywords='',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://pypi.python.org/pypi/plone.app.contentlisting',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.uuid',
+    ],
+    extras_require={
         'test': ['plone.app.testing'],
-      },
-      entry_points='''
-      [z3c.autoinclude.plugin]
-      target = plone
-      ''',
-      )
+    },
+    entry_points='''
+    [z3c.autoinclude.plugin]
+    target = plone
+    ''',
+)
