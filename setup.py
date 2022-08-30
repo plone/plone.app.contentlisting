f47a0f9c4c7b7166f6fb71aafcb0fb0fb93e5e63
Maurits van Rees <maurits@vanrees.org>
2022-08-30 22:32:02 +0200

    Added Python 3.10 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="content list Plone",
     author="Plone Foundation",
